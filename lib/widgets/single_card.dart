56492786ceaeaa7a4d88f8e090b59b6af94690f4
Phils Garcia <eivanphils@gmail.com>
2022-07-31 18:29:30 -0400

    Agregado blur en el SingleCard Widget

--- lib/widgets/single_card.dart
+++ lib/widgets/single_card.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SingleCard extends StatelessWidget {
@@ -16,23 +18,31 @@ class SingleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color.fromRGBO(62, 66, 107, 0.7),
-      ),
       margin: const EdgeInsets.all(15),
-      height: 180,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            child: Icon(icon, color: Colors.white, size: 35),
-            radius: 30,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color.fromRGBO(62, 66, 107, 0.7),
+            ),
+            height: 180,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: color,
+                  child: Icon(icon, color: Colors.white, size: 35),
+                  radius: 30,
+                ),
+                const SizedBox(height: 10),
+                Text(title, style: TextStyle(color: color, fontSize: 18),)
+              ],
+            ),
           ),
-          const SizedBox(height: 10),
-          Text(title, style: TextStyle(color: color, fontSize: 18),)
-        ],
+        ),
       ),
     );
   }
